2fddbfd5b540be57fdecac7bbf31bab8e37158b0
Yasin Emre OZBARUT <ysnemreozbarut@gmail.com>
2019-07-31 10:33:50 +0300

    Initial commit 2

---
+++ KafkaLogListener/src/main/java/com/teb/kafkaloglistener/controller/KafkaController.java
@@ -0,0 +1,25 @@
+package com.teb.kafkaloglistener.controller;
+
+import com.teb.kafkaloglistener.services.Producer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+
+    private final Producer producer;
+
+    @Autowired
+    KafkaController(Producer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping(value = "/publish")
+    public void sendMessage2KafkaTopic (@RequestParam("message") String message) {
+        producer.sendMessage(message);
+    }
+}
